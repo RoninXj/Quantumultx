a701e61a0f981b598d6f4c5a84f91e34a42e2c4b
RoninXj <48503899+RoninXj@users.noreply.github.com>
2022-05-16 10:38:49 +0800

    Create aqc.js

---
+++ aqc.js
@@ -0,0 +1,15 @@
+/*
+10.18
+
+软件：  爱企查
+收益：  每天250左右爱豆
+注意事项 ： 运行js时不要打开软件app
+
+获取ck：  打开软件点击我的，右上角签到即可获取
+
+重写：
+QX重写：https://aiqicha.baidu.com/zxcenter url script-request-header aqc.js
+
+主机名：hostname = aiqicha.baidu.com
+
+*/
