cc8cf7b496f773f30f65ca0de9e2fa07d7615882
RoninXj <48503899+RoninXj@users.noreply.github.com>
2022-12-20 20:52:44 +0800

    Update gqcqwx.js

--- gqcqwx.js
+++ gqcqwx.js
@@ -1,16 +1,7 @@
-async function GetRewrite() {
- if ($request.url.indexOf("live/home/tab-bar-status.json") > -1) {
-  const ck = $request.headers.token
-  if (ckStr) {
-   if (ckStr.indexOf(ck) == -1) { // 找不到返回 -1
-    ckStr = ckStr + "\n" + ck;
-    $.setdata(ckStr, "gqcqwxcookie");
-    ckList = ckStr.split("\n");
-    $.msg($.name + ` 获取第${ckList.length}个 ck 成功: ${ck} ,不用请自行关闭重写!`);
-   }
-  } else {
-   $.setdata(ck, "gqcqwxcookie");
-   $.msg($.name + ` 获取第1个 ck 成功: ${ck} ,不用请自行关闭重写!`);
-  }
- }
+$prefs.setValueForKey($request.headers.token, 'gqcqwxcookie');
+
+if (typeof $prefs.valueForKey('gqcqwxcookie') !== "undefined"){
+    $notify('广汽传祺小程序', token 获取成功,数据已存储!, $request['headers']['token']);
 }
+
+$done();
