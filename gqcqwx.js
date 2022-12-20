59f99cbbae1f4f9216907f6f953068c6759c09e4
RoninXj <48503899+RoninXj@users.noreply.github.com>
2022-12-20 20:56:48 +0800

    Update gqcqwx.js

--- gqcqwx.js
+++ gqcqwx.js
@@ -1,7 +1,7 @@
 $prefs.setValueForKey($request.headers.token, 'gqcqwxcookie');
 
 if (typeof $prefs.valueForKey('gqcqwxcookie') !== "undefined"){
-    $notify('广汽传祺小程序', token 获取成功,数据已存储!, $request['headers']['token']);
+    $notify('广汽传祺小程序', gqcqwxcookie 获取成功,数据已存储!, $request['headers']['token']);
 }
 
 $done();
